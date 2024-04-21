da13f85600dbb298f4ff90c9e99cf056a3b0519a
wkk123455 <138169702+wkk123455@users.noreply.github.com>
2024-04-21 00:50:03 -0700

    Create user_config.py

---
+++ user_config.py
@@ -0,0 +1,19 @@
+source_file = "user_demo.txt"
+final_file = "user_result.txt"
+favorite_list = [
+    "广东珠江",
+    "广东体育",
+    "广东卫视",
+    "浙江卫视",
+    "湖南卫视",
+    "翡翠台",
+]
+favorite_page_num = 3
+default_page_num = 2
+urls_limit = 10
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 30
+ipv_type = "ipv4"
+domain_blacklist = ["epg.pw"]
+url_keywords_blacklist = []
